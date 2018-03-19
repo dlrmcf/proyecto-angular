1a9659ec16cd2d8db90b116e9058e03dc92d6d51
dlrmcf <davidrodrica@gmail.com>
2018-03-19 22:26:36 +0100

    todois archivos

---
+++ src/app/operativas/operativas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operativas',
+  templateUrl: './operativas.component.html',
+  styleUrls: ['./operativas.component.css']
+})
+export class OperativasComponent {
+  title = 'app';
+}
